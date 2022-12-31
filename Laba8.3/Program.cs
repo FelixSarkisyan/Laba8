e61a8594b12809006e9d397e9cb505eff5fcf5c4
FelixSarkisyan <113985516+FelixSarkisyan@users.noreply.github.com>
2022-12-31 14:41:58 +0500

    Task3

---
+++ Laba8.3/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+
+namespace Anagrams
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] array = new string[] { "code", "doce", "ecod", "framer", "frame" };
+            string[] changedArray = DeleteAnagramsFromArray(array);
+            foreach (string s in changedArray)
+                Console.WriteLine(s);
+
+        }
+
+        private static string[] DeleteAnagramsFromArray(string[] array)
+        {
+            List<string> list = new()
+            {
+                array[0]
+            };
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (!array[i].IsAnagramTo(array[i - 1])) list.Add(array[i]);
+            }
+
+            list.Sort();
+            return list.ToArray();
+        }
+
+    }
+    public static class StringExtension
+    {
+        public static bool IsAnagramTo(this string word1, string word2)
+        {
+            bool result = true;
+            char[] chars1 = word1.ToCharArray();
+            char[] chars2 = word2.ToCharArray();
+            AlphabetIndexComparer comparer = new();
+            chars1.Sort(comparer);
+            chars2.Sort(comparer);
+            if (chars1.Length != chars2.Length)
+                return false;
+            else
+            {
+                for (int i = 0; i < chars1.Length; i++)
+                {
+                    if (chars1[i] != chars2[i])
+                        result = false;
+                }
+
+            }
+
+            return result;
+        }
+
+    }
+    public static class ArrayExtensions
+    {
+        public static void Sort(this Array array, IComparer comparer)
+        {
+            for (int i = array.Length - 1; i > 0; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    object element1 = array.GetValue(j - 1);
+                    object element2 = array.GetValue(j);
+                    if (comparer.Compare(element1, element2) > 0)
+                    {
+                        object temporary = array.GetValue(j);
+                        array.SetValue(array.GetValue(j - 1), j);
+                        array.SetValue(temporary, j - 1);
+                    }
+
+                }
+
+            }
+
+        }
+
+    }
+    class AlphabetIndexComparer : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            char c1 = (char)x;
+            char c2 = (char)y;
+            return c1.CompareTo(c2);
+        }
+
+    }
+}
